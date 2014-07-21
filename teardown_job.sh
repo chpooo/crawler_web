d09120569ad9c5692ce7be0e03b148de170c653f
chpooo <changpeng3336@163.com>
2014-07-21 21:14:52 +0800

    add piconv for some tasks

--- teardown_job.sh
+++ teardown_job.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 curl -v -d "action=teardown" -k -u admin:admin --anyauth --location "https://""$1"":8443/engine/job/""$2" 
+if [ "$2" = "www_evpartner_com_news" ]
+then
+	ssh root@$1 "cd /root/zju_chpoo/crawler/jobs/"$2"/latest/warcs/;piconv -f gb2312 -t utf-8 ./* > 1.warc"
+fi
+
+if [ "$2" = "www_ddqc_cc_news" ]
+then
+	ssh root@$1 "cd /root/zju_chpoo/crawler/jobs/"$2"/latest/warcs/;piconv -f gb2312 -t utf-8 ./* > 1.warc"
+fi
+
+
+if [ "$2" = "www_evdavs_com_news" ]
+then
+	ssh root@$1 "cd /root/zju_chpoo/crawler/jobs/"$2"/latest/warcs/;piconv -f gb2312 -t utf-8 ./* > 1.warc"
+fi
+
+
+if [ "$2" = "auto_sina_com_cn" ]
+then
+	ssh root@$1 "cd /root/zju_chpoo/crawler/jobs/"$2"/latest/warcs/;piconv -f gb2312 -t utf-8 ./* > 1.warc"
+fi
+
+
+if [ "$2" = "www_sovev_com_news" ]
+then
+	ssh root@$1 "cd /root/zju_chpoo/crawler/jobs/"$2"/latest/warcs/;piconv -f gb2312 -t utf-8 ./* > 1.warc"
+fi
